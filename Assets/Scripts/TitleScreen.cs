5fa0f307182f307e04d6940910b235641737b945
Roba Abbajabal <32786155+abbajabalroba@users.noreply.github.com>
2019-05-27 00:56:19 -0500

    A new Unity game!?!? How is this possible

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour
+{   
+    public Button startSinglePlayer;
+
+    void Start()
+    {
+        startSinglePlayer.onClick.AddListener(StartSinglePlayer);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void StartSinglePlayer()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
